8ad2ded05171cbabe03c141c94fb313a490b9819
Jacob <jacobpayne007@gmail.com>
2024-04-02 22:02:19 -0500

    Test is broken, shouldn't cause undefined when break is added

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if(ride <=400){
+    return "This one is on me!"
+  }
+  else if(ride <=2000 && ride >=400){
+    return "That will be twenty bucks."
+  }
+  else if (ride >=2000 && ride <=2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else{
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? 'Ok, sounds good.' : "No go.";
+  
+}
+const tip = "";
+let thankYouMessage;
+debugger
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+switchOnCharmFromTip(tip)
+console.log(thankYouMessage)
